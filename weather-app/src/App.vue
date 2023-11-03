311deb9472698b0617e19f6e1d7ef0f158871e7e
Ian Chirwa <chirwaian7@gmail.com>
2023-11-03 14:43:27 +0200

    deleted default component

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -4,7 +4,7 @@
       <h1 class="mb-5">Weather App</h1>
       <div class="d-flex justify-content-center h-100">
         <div class="searchbar w-50 mx-2">
-          <input type="text" class="input form-control" placeholder="Enter a City">
+          <input type="text" class="input form-control" v-model="city" placeholder="Enter a City">
         </div>
         <button class="btn-search btn btn-primary">Search <i class="fas fa-search"></i></button>
       </div>
@@ -18,6 +18,19 @@ export default (await import('vue')).defineComponent({
   name: 'App',
   components: {
     
+  },
+  data(){
+    return {
+      city: '',
+      showWeather: false,
+    }
+  },
+  methods: {
+    async searchWeather (){
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    }
   }
 })
 </script>
