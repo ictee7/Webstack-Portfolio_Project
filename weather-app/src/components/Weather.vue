815113d178f082a4d6e481e5d4d9897cbe6aa949
Ian Chirwa <chirwaian7@gmail.com>
2023-11-03 15:26:42 +0200

    added style for maincard and second card

--- weather-app/src/components/Weather.vue
+++ weather-app/src/components/Weather.vue
@@ -52,6 +52,94 @@
   </script>
   
   <style>
+
+  body{
+    background-color: #343d4b;
+  }
+  .weather-temp{
+    margin: 0;
+    font-weight: 700;
+    font-size: 4em;
+  }
+  h2.mb-1.day{
+    font-size: 3rem;
+    font-weight: 400;
+  }
+  .main-div{
+    border-radius: 20px;
+    color: #fff;
+    background-image: url('../../images/img.jpg');
+    background-size: cover;
+
+    background-color: rgba (0,0,0,0.5);
+    background-repeat: no-repeat;
+    
+  }
+  .temp{
+    position:absolute;
+    bottom: 0;
+  }
+  .main-div:hover{
+    transform: scale(1.1);
+    transition: transform 0.5s ease;
+    z-index: 1;
+
+  }
+
+  .card-2 {
+    background-color: #212730 !important;
+    border-radius: 20px;
+
+  }
+  .card-details {
+    margin-left: 19px;
+  }
+  .h1_left {
+    position: absolute;
+    bottom: 25px;
+    left: 16px;
+    font-size: 3vw;
+    line-height: 1.2;
+  }
+  .h3_left{
+    position: absolute;
+    left: 16px;
+    font-size: 2vw;
+    line-height: 0.5;
+  }
+  .h3_left small {
+    font-size: 1rem;
+  }
+  table{
+    position: relative;
+    left: 15px;
+    border-collapse: separate;
+    border-spacing: 15px;
+    width: 85%;
+    text-align: left;
+    max-width: 600px;
+    margin: 0 auto;
+
+  }
+  th,
+  td {
+    font-size: 18px;
+    color: #fff;
+  }
+  td{
+    text-align: right;
+  }
+  table,
+  tr:hover{
+    color: red; 
+  }
+  .change_btn {
+    background-image: linear-gradient(to right,cyan magenta);
+  }
+  .change_btn:hover{
+    transform: scale(0.9);
+    transition: transform 0.1s ease;
+  }
  
   </style>
   
