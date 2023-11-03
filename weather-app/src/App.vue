d5d94fa04b982ec92bf0c7bb40275fdf4cf031a1
Ian Chirwa <chirwaian7@gmail.com>
2023-11-03 14:21:34 +0200

    added search bar

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -2,18 +2,23 @@
   <div class="app">
     <div class="header container h-100 p-5">
       <h1 class="mb-5">Weather App</h1>
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar w-50 mx-2">
+          <input type="text" class="input form-control" placeholder="Enter a City">
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
-export default {
+export default (await import('vue')).defineComponent({
   name: 'App',
   components: {
     
   }
-}
+})
 </script>
 
 <style>
